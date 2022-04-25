3378fa6d64e9c48a896351dce3487c8137548fd8
jesse zenon <jesse.zenon@gmail.com>
2022-04-25 14:58:56 -0500

    commit tree f4cf9347f2764b5d69bea6ddbc507890880edc89

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride < 400) {
+    return "This one is on me!";
+  } else if (ride >= 400 && ride < 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city) {
+  // Write your code here!
+  city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let response;
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+  }
 }
---
+++ newTry.js
@@ -0,0 +1,15 @@
+function scuberGreetingForFeet(ride) {
+  if (ride < 400) {
+    return "This one is on me!";
+  } else if (ride < 2000) {
+    return "20";
+  } else if (ride < 2500) {
+    return "30";
+  } else if (ride > 2500) {
+    return "no";
+  }
+}
+
+function ternaryCheckCity() {}
+
+function switchOnCharmFromTip() {}
